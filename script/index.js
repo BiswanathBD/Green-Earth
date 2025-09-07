44183f115e2122445cbcf96fec295c3373ccf90c
BiswanathBD <biswanath.sarker.bd@gmail.com>
2025-09-07 14:46:03 +0600

    item by categories features added

--- script/index.js
+++ script/index.js
@@ -24,7 +24,11 @@ const showCategories = (categories) => {
   const categoriesContainer = getById("category-container");
   categories.forEach((category) => {
     const categoryBtn = document.createElement("li");
-    categoryBtn.classList.add("bg-green-100", "md:bg-transparent");
+    categoryBtn.classList.add(
+      "bg-green-100",
+      "md:bg-transparent",
+      "category-btn"
+    );
     categoryBtn.id = `${category.id}`;
     categoryBtn.innerText = `${category.category_name}`;
     categoriesContainer.append(categoryBtn);
@@ -63,3 +67,43 @@ const showPlants = (items) => {
     itemContainer.append(itemCard);
   });
 };
+
+// item by category button
+const categoriesBtn = getById("categories");
+categoriesBtn.addEventListener("click", (e) => {
+  if (e.target.classList.contains("category-btn")) {
+    const btnId = e.target.id;
+    const url = `https://openapi.programming-hero.com/api/category/${btnId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((items) => showPlantsCategories(items.plants));
+    const showPlantsCategories = (items) => {
+      const itemContainer = getById("items-container");
+      itemContainer.innerHTML = "";
+      items.forEach((item) => {
+        const itemCard = document.createElement("div");
+        itemCard.classList.add(
+          "flex",
+          "flex-col",
+          "gap-1",
+          "justify-between",
+          "bg-white",
+          "p-3",
+          "rounded-xl",
+          "shadow-lg"
+        );
+        itemCard.innerHTML = `
+    <div class="aspect-[5/3] w-full overflow-hidden rounded-t-md"><img class="w-full h-full object-cover" src="${item.image}" alt=""></div>
+      <p class="font-semibold text-lg">${item.name}</p>
+      <p class="text-[10px] text-gray-600">${item.description}</p>
+      <div class="flex justify-between items-center">
+      <button class="px-2 py-1 text-xs bg-green-100 text-green-700 rounded-md">${item.category}</button>
+      <p class="text-lg">৳<span id="price" class="font-semibold text-sm"> ${item.price}</span></p>
+      </div>
+    <button id="add-btn" class="text-white px-4 py-2 mt-2 bg-green-700 text-sm font-semibold rounded-lg hover:bg-green-700/80">Add to Cart</button>
+    `;
+        itemContainer.append(itemCard);
+      });
+    };
+  }
+});
