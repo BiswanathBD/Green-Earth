e450cbd5eed9e5681a99b481df359fddfc49a4fb
BiswanathBD <biswanath.sarker.bd@gmail.com>
2025-09-07 12:49:23 +0600

    all items shown by defult

--- script/index.js
+++ script/index.js
@@ -14,3 +14,51 @@ menuBar.addEventListener("click", () => {
     menuBar.classList.replace("fa-bars-staggered", "fa-bars");
   }
 });
+
+// fetch categories data and add to ui
+fetch("https://openapi.programming-hero.com/api/categories")
+  .then((res) => res.json())
+  .then((categories) => showCategories(categories.categories));
+
+const showCategories = (categories) => {
+  const categoriesContainer = getById("category-container");
+  categories.forEach((category) => {
+    const categoryBtn = document.createElement("li");
+    categoryBtn.id = `${category.id}`;
+    categoryBtn.innerText = `${category.category_name}`;
+    categoriesContainer.append(categoryBtn);
+  });
+};
+
+// fetch all plants and show on ui
+fetch("https://openapi.programming-hero.com/api/plants")
+  .then((res) => res.json())
+  .then((items) => showPlants(items.plants));
+
+const showPlants = (items) => {
+  const itemContainer = getById("items-container");
+  items.forEach((item) => {
+    const itemCard = document.createElement("div");
+    itemCard.classList.add(
+      "flex",
+      "flex-col",
+      "gap-1",
+      "justify-between",
+      "bg-white",
+      "p-3",
+      "rounded-xl",
+      "shadow-md"
+    );
+    itemCard.innerHTML = `
+    <div class="aspect-[4/3] w-full overflow-hidden rounded-md"><img class="w-full h-full object-cover" src="${item.image}" alt=""></div>
+      <p class="font-semibold text-lg">${item.name}</p>
+      <p class="text-sm text-gray-600">${item.description}</p>
+      <div class="flex justify-between items-center">
+      <button class="px-2 pb-1 text-xs bg-green-100 text-green-700 rounded-full">${item.category}</button>
+      <p class="text-lg">৳<span id="price" class="font-semibold text-sm"> ${item.price}</span></p>
+      </div>
+    <button id="add-btn" class="text-white px-3 py-1 bg-green-700 text-sm font-semibold rounded-full">Add to Cart</button>
+    `;
+    itemContainer.append(itemCard);
+  });
+};
